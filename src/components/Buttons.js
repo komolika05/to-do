dc864354bbc5f8b9c7ed2a5d8095bc9408d4ce5d
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-06-27 10:43:23 +0530

    done basic styling

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,20 @@
+import React, {useState, useEffect} from 'react';
+
 const Buttons = () => {
+    const [isCompleteScreen , setIsCompleteScreen] = useState(false)
     return (
-        <div className="btn-area">
-            <button>Todo</button>
-            <button>Completed</button>
-        </div>
+        <>
+            <div className="btn-area">
+                <button className={`secondaryBtn ${isCompleteScreen === false && 'active'}`} onClick={() => setIsCompleteScreen (false)}>Todo</button>
+                <button className={`secondaryBtn ${isCompleteScreen === true && 'active'}`} onClick={() => setIsCompleteScreen (true)}>Completed</button>
+            </div>
+            <div className="todo-list">
+                <div className="todo-list-item">
+                    <h3>Task 1</h3>
+                    <p>Description</p>
+                </div>
+            </div>
+        </>
     )
 }
 
